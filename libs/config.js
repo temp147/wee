35de09fa62d2834136d0301d611e637ff9a6e926
temp147 <temp147@163.com>
2015-06-16 17:42:45 +0800

    add wechat api

---
+++ libs/config.js
@@ -0,0 +1,17 @@
+/**
+ * Created by root on 6/16/15.
+ */
+module.exports = {
+    secret: 'secret',
+    port: 3000,
+    mp: {
+        appid: 'your appid',
+        token: 'your token',
+        encodingAESKey: 'AXY7qO8vrcNtQh0GjFyUjDquMDD53z4MJhnS6eyBHz1'
+    },
+    corp: {
+        token: 'YOUR token',
+        encodingAESKey: 'YOUR encodingAESKey',
+        corpId: 'YOUR corpId'
+    }
+};
