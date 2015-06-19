c0d1e2851f2cb24c095bfe0e7460a8b7157cbc7f
temp147 <temp147@163.com>
2015-06-19 17:53:57 +0800

    split file

--- libs/config.js
+++ libs/config.js
@@ -2,7 +2,7 @@
  * Created by root on 6/16/15.
  */
 module.exports = {
-    secret: 'secret',
+    secret: 'e474867b3ecb08caa14d22eb94787b74',
     port: 3000,
     mp: {
         appid: 'wx0ce2cd3f023755bf',
--- routes.js
+++ routes.js
@@ -10,19 +10,31 @@ var config = require('./libs/config');
 
 var fs = require('fs');
 
-var api = new wechatapi('appid', 'secret', function (callback) {
-    // 传入一个获取全局token的方法
-    fs.readFile('access_token.txt', 'utf8', function (err, txt) {
-        if (err) {return callback(err);}
-        callback(null, JSON.parse(txt));
-    });
-}, function (token, callback) {
-    // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
-    // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
-    fs.writeFile('access_token.txt', JSON.stringify(token), callback);
+var api = new wechatapi(config.mp.appid,config.secret
+//    ,
+//    function (callback) {
+//    // 传入一个获取全局token的方法
+//    fs.readFile('access_token.txt', 'utf8', function (err, txt) {
+//        if (err) {return callback(err);}
+//        callback(null, JSON.parse(txt));
+//    });
+//},
+//    function (token, callback) {
+//    // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
+//    // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
+//        fs.writeFile('access_token.txt', JSON.stringify(token), function (err, txt) {
+//            if (err) {
+//                return callback(err);
+//            }
+//            callback(null,txt);
+//        })
+//}
+);
+
+api.getLatestToken(function(err,token){
+    console.log(err);
+    console.log(token);
 });
-
-
 
 //handle the http options method,response 204
 function option (req,res){
@@ -32,6 +44,9 @@ function option (req,res){
 
 module.exports = function(app){
     app.use('/wechat',wechat(config.mp,function(req,res,next){
+//        console.log(req);
+//        console.log(req.query.code);
+//        api.saveToken(req.query.code,function(){});
         var menu = fs.readFileSync('./libs/wechat-menu.json');
         if(menu){
             menu=JSON.parse(menu)
