04afbfecc5760dbd38d1b379cc6bdf896cce67d4
temp147 <temp147@163.com>
2015-06-19 18:20:05 +0800

    split file

--- routes.js
+++ routes.js
@@ -10,25 +10,20 @@ var config = require('./libs/config');
 
 var fs = require('fs');
 
-var api = new wechatapi(config.mp.appid,config.secret
-//    ,
-//    function (callback) {
-//    // 传入一个获取全局token的方法
-//    fs.readFile('access_token.txt', 'utf8', function (err, txt) {
-//        if (err) {return callback(err);}
-//        callback(null, JSON.parse(txt));
-//    });
-//},
-//    function (token, callback) {
-//    // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
-//    // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
-//        fs.writeFile('access_token.txt', JSON.stringify(token), function (err, txt) {
-//            if (err) {
-//                return callback(err);
-//            }
-//            callback(null,txt);
-//        })
-//}
+var api = new wechatapi(config.mp.appid,config.secret,
+    function (callback) {
+    // 传入一个获取全局token的方法
+    fs.readFile('access_token.txt', 'utf8', function (err, txt) {
+//        console.log(JSON.parse(txt));
+        if (err) {return callback(err);}
+        callback(null, JSON.parse(txt));
+    });
+},
+    function (token, callback) {
+    // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
+    // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
+        fs.writeFile('access_token.txt', JSON.stringify(token),callback)
+}
 );
 
 api.getLatestToken(function(err,token){
