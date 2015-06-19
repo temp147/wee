c950d1566be5de1216ae433f1fa20846ab30d30f
temp147 <temp147@163.com>
2015-06-19 13:52:42 +0800

    split file

--- routes.js
+++ routes.js
@@ -4,8 +4,13 @@
 
 //load wechat intere
 var wechat = require('wechat');
+var wechatapi = require('wechat-api');
 //load config file
 var config = require('./libs/config');
+
+var fs = require('fs');
+
+var api = new wechatapi(config.mp.appid,config.mp.encodingAESKey);
 
 
 //handle the http options method,response 204
@@ -16,6 +21,11 @@ function option (req,res){
 
 module.exports = function(app){
     app.use('/wechat',wechat(config.mp,function(req,res,next){
+        var menu = fs.readFileSync('./libs/wechat-menu.json');
+        if(menu){
+            menu=JSON.parse(menu)
+        }
+        api.createMenu(menu,function(err,result){});
         var message = req.weixin;
         if(message.MsgType == 'text'){
             res.reply({type:"text",content:"you input"+message.Content});
